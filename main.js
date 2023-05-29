cf9dc3041522127c822687a73f308599ef865ac5
Antoni Lueddeke <123427446+alueddeke@users.noreply.github.com>
2023-05-29 13:43:23 -0400

    completed functionality and local storage

---
+++ main.js
@@ -0,0 +1,135 @@
+// if there are any todos in local storage, go fetch them
+//to view, f12 in browser, application, local storage
+//todos is now a global variable
+window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const nameInput = document.querySelector("#name");
+  const newTodoForm = document.querySelector("#new-todo-form");
+
+  const username = localStorage.getItem("username") || "";
+
+  nameInput.value = username;
+
+  nameInput.addEventListener("change", (e) => {
+    localStorage.setItem("username", e.target.value);
+  });
+
+  newTodoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todo = {
+      content: e.target.elements.content.value,
+      category: e.target.elements.category.value,
+      done: false,
+      createdAt: new Date().getTime(),
+    };
+    //add a new todo to array
+    todos.push(todo);
+
+    //turn todos array into JSON string, store string in local storage
+    //local storage can only store primitive values
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    //reset the fill on the todo form and uncheck the box
+    e.target.reset();
+
+    DisplayTodos();
+  });
+
+  DisplayTodos();
+});
+
+function DisplayTodos() {
+  const todoList = document.querySelector("#todo-list");
+  todoList.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
+
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+    const content = document.createElement("div");
+    const actions = document.createElement("div");
+    const edit = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    input.type = "checkbox";
+    input.checked = todo.done;
+    span.classList.add("bubble");
+
+    //makes it blue or pink
+    if (todo.category == "personal") {
+      span.classList.add("personal");
+    } else {
+      span.classList.add("business");
+    }
+
+    content.classList.add("todo-content");
+    actions.classList.add("actions");
+    edit.classList.add("edit");
+    deleteButton.classList.add("delete");
+
+    //creating a new element within the HTML
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+    edit.innerHTML = "Edit";
+    deleteButton.innerHTML = "Delete";
+
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    actions.appendChild(deleteButton);
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+
+    todoList.appendChild(todoItem);
+
+    //FOR COMPLETED TASKS
+    if (todo.done) {
+      todoItem.classList.add("done");
+    }
+
+    if (todo.done) {
+      todoItem.classList.add("done");
+    }
+
+    input.addEventListener("change", (e) => {
+      todo.done = e.target.checked;
+      localStorage.setItem("todos", JSON.stringify(todos));
+
+      if (todo.done) {
+        todoItem.classList.add("done");
+      } else {
+        todoItem.classList.remove("done");
+      }
+
+      DisplayTodos();
+    });
+
+    edit.addEventListener("click", (e) => {
+      const input = content.querySelector("input");
+      //make it editable
+      input.removeAttribute("readonly");
+      //highlight, bring you to input field
+      input.focus();
+      //click outside input field, it will stop editing
+      input.addEventListener("blur", (e) => {
+        input.setAttribute("readonly", true);
+        todo.content = e.target.value;
+        localStorage.setItem("todos", JSON.stringify(todos));
+        DisplayTodos();
+      });
+    });
+
+    deleteButton.addEventListener("click", (e) => {
+      //remove what we have clicked
+      todos = todos.filter((t) => t != todo);
+
+      //update display and local storage
+      localStorage.setItem("todos", JSON.stringify(todos));
+      DisplayTodos();
+    });
+  });
+}
